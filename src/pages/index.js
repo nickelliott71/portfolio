c0ea4a518eb2fb985af56ce60d04c40ac46102d5
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2023-09-15 13:52:29 +0100

    temp style fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,7 @@ import { Container } from '../layout/LayoutStyles'
 
 const Home = () => {
   return (
-    <div style="text-align: center;
-    margin-top: 2em;
-    font-size: 2em;">
+    <div style={{textAlign: 'center', marginTop: '2em', fontSize: '2em'}}>
     <Container>
       <Image
         src="/images/website-logo.png"
