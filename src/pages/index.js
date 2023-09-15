f4955c2495be657db5df3f7fba624b183d262cc5
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2023-09-15 13:41:50 +0100

    Dummy home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,19 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
-import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+import Image from 'next/image';
+
 
 const Home = () => {
   return (
     <Layout>
       <Section grid>
-        <Hero />
-        <BgAnimation />
+      <Image
+        src="/images/website-logo.png"
+        width={200}
+        height={200}
+        alt=""
+      />
       </Section>
-      <Projects />
-      <Technologies />
-      <Timeline />
-      <Acomplishments />
     </Layout>
   );
 };
