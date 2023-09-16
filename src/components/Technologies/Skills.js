862c33b4b74d40a078eb4861473fcc167a0f5758
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2023-09-16 12:46:58 +0100

    Skills update

--- src/components/Technologies/Skills.js
+++ src/components/Technologies/Skills.js
@@ -14,68 +14,31 @@ export const Skills = [
     slug: "ideation",
     Component: RiFlutterFill,
     title: "Ideation & design",
-    Description: () => <><p>Help teams take a problem or insight and work through solutions that will elevate you above your competitors.</p></>,
+    Description: () => <><p>Help engage your whole product and development team with a problem or insight. Ideate towards solutions that will elevate you above your competitors.</p></>,
   },
   {
-    slug: "kotlin",
+    slug: "prototyping",
     Component: TbBrandKotlin,
-    title: "Kotlin",
+    title: "Prototyping",
     Description: () => <>Android apps and Lambda functions</>,
   },
   {
-    slug: "dart",
+    slug: "testing",
     Component: SiDart,
-    title: "Dart",
+    title: "Usability testing",
     Description: () => <>Flutter apps only</>,
   },
   {
-    slug: "java",
+    slug: "ds",
     Component: DiJava,
-    title: "Java",
+    title: "Design systems",
     Description: () => <>Android apps and Lambda functions</>,
   },
 
   {
-    slug: "html",
+    slug: "accessibility",
     Component: DiHtml5,
-    title: "HTML",
+    title: "Accessibility",
     Description: () => <>Static webpages and portfolio projects</>,
-  },
-  {
-    slug: "css",
-    Component: SiCss3,
-    title: "CSS",
-    Description: () => <>Styling of my webpages</>,
-  },
-
-  {
-    slug: "sql",
-    Component: SiMysql,
-    title: "MySQL",
-    Description: () => <>Storing client and user data</>,
-  },
-  {
-    slug: "aws",
-    Component: SiAmazonaws,
-    title: "AWS Lambda",
-    Description: () => <>Lambda functions for creating APIs</>,
-  },
-  {
-    slug: "terminal",
-    Component: DiTerminal,
-    title: "Bash",
-    Description: () => <>Ease of life and build scripts</>,
-  },
-  {
-    slug: "firebase",
-    Component: SiFirebase,
-    title: "Firebase",
-    Description: () => <>Authentication, database and analytics</>,
-  },
-  {
-    slug: "git",
-    Component: SiGit,
-    title: "Git",
-    Description: () => <>Code management and open source contributions</>,
-  },
+  }
 ];
