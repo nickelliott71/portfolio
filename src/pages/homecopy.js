2876231aeacde8a8b857fed69d88b52751fbf280
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2023-09-16 13:12:55 +0100

    Adding image

--- src/pages/homecopy.js
+++ src/pages/homecopy.js
@@ -6,12 +6,19 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+import Image from 'next/image';
 
 const HomeCopy = () => {
   return (
     <Layout>
       <Section grid>
         <Hero />
+        <Image
+        src="/images/website-logo.png"
+        width={200}
+        height={200}
+        alt=""
+      />
         <BgAnimation />
       </Section>
       <Technologies />
