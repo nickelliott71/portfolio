088018f2f1c57003eaa6abd6bf48759164698746
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2023-09-25 14:48:23 +0100

    Stripped contact right back

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,24 +1,6 @@
-import nodemailer from 'nodemailer';
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
+
+  res.status(200).json({ name: 'John Doe' })
   
-  console.log("here 1")
-
-  if (req.method === 'POST') {
-
-    console.log("here 2")
-    try {
-      const { name, email, phoneNumber, message } = req.body;
-
-    
-
-      res.status(200).json({ message: 'Email sent successfully in api' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error sending email in api' });
-    }
-  } else {
-    res.status(405).end();
-    console.log("here 405")
-  }
 }
