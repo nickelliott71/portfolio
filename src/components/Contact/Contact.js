5dd580d44ec5b575c4a39c9f144da29029baa21e
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2023-09-25 13:55:24 +0100

    Email fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -23,7 +23,7 @@ const Contact = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch('/api/contact', {
+      const response = await fetch('/pages/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
