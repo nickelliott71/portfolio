1d98849ad985e5a7e8b10bf9315deb0d91f7a5b4
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2023-09-25 14:58:09 +0100

    api change

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,6 +1,6 @@
 
-export default function handler(req, res) {
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+export default (req, res) => {
   res.status(200).json({ name: 'John Doe' })
-  
 }
