493b09b582b612c8d0d424c9de23a6d0dc322167
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2023-09-27 13:32:44 +0100

    Hero tweak

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -10,6 +10,13 @@ export const LeftSection = styled.div`
     margin: 0 auto;
   }
   @media ${(props) => props.theme.breakpoints.md} {
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+
+    margin: 0 auto;
+  }
+  @media ${(props) => props.theme.breakpoints.lg} {
     width: 100%;
     display: flex;
     flex-direction: column;
