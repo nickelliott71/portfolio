037265314114ce322e59de3771ddc2ed06758e6a
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2023-09-27 14:18:26 +0100

    Footer tweak

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillTwitterCircle } from 'react-icons/ai';
 import { Link } from '../../styles/GlobalComponents';
 import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SloganLink, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   const today = new Date();
