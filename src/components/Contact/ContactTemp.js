b9b55b892b7a4a3b85b86b15ca028fd1f93ba579
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2023-09-28 10:02:57 +0100

    Text tweak

--- src/components/Contact/ContactTemp.js
+++ src/components/Contact/ContactTemp.js
@@ -10,7 +10,7 @@ const ContactTemp = () => {
     <Section id="contact">
       <SectionTitle>Contact</SectionTitle>
       <SectionText>
-        If you think I can help your business, please send me a message on <SectionLink href="https://www.linkedin.com/in/nickelliott/">LinkedIn</SectionLink> and we'll set up a no obligation chat.  Thanks!
+        If you think I can help your business, please send me a message on <SectionLink href="https://www.linkedin.com/in/nickelliott/">LinkedIn</SectionLink> to set up a no obligation chat.  Thanks!
       </SectionText>
     </Section>
   );
